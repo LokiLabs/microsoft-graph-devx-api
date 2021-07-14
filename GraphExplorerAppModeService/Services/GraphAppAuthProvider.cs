fa284e8b370ad436bab4af1cfb16a554f0379e6b
Omer Abubaker <omerabubakerca@gmail.com>
2021-07-14 12:50:21 -0400

    Set up end point for graph explorer application mode proxy

---
+++ GraphExplorerAppModeService/Services/GraphAppAuthProvider.cs
@@ -0,0 +1,19 @@
+﻿using GraphExplorerAppModeService.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GraphExplorerAppModeService.Services
+{
+    public class GraphAppAuthProvider : IGraphAppAuthProvider
+    {
+        public GraphAppAuthProvider(IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ GraphWebApi/Controllers/GraphExplorerAppModeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using GraphExplorerAppModeService.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphWebApi.Controllers
+{
+    [ApiController]
+    public class GraphExplorerAppModeController : ControllerBase
+    {
+        [Route("api/[controller]/{*all}")]
+        [Route("graphproxy/{*all}")]
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(string all)
+        {
+            return await ProcessRequestAsync("GET", all, null).ConfigureAwait(false);
+        }
+
+        [Route("api/[controller]/{*all}")]
+        [Route("graphproxy/{*all}")]
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(string all, [FromBody] object body)
+        {
+            return await ProcessRequestAsync("POST", all, body).ConfigureAwait(false);
+        }
+
+        [Route("api/[controller]/{*all}")]
+        [Route("graphproxy/{*all}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(string all)
+        {
+            return await ProcessRequestAsync("DELETE", all, null).ConfigureAwait(false);
+        }
+
+        [Route("api/[controller]/{*all}")]
+        [Route("graphproxy/{*all}")]
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(string all, [FromBody] object body)
+        {
+            return await ProcessRequestAsync("PUT", all, body).ConfigureAwait(false);
+        }
+
+        [Route("api/[controller]/{*all}")]
+        [Route("graphproxy/{*all}")]
+        [HttpPatch]
+        public async Task<IActionResult> PatchAsync(string all, [FromBody] object body)
+        {
+            return await ProcessRequestAsync("PATCH", all, body).ConfigureAwait(false);
+        }
+
+        private async Task<IActionResult> ProcessRequestAsync(string method, string all, object content)
+        {
+            return Ok();
+        }
+    }
+}
